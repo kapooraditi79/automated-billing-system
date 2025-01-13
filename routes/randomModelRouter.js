0eb74ad71eadae951970ae41bdc93c6ca48fbcfe
kapooraditi79 <thinktank7997@gmail.com>
2025-01-13 20:08:00 +0530

    Fourth Changes made. Added The Add to Cart Feature

--- routes/randomModelRouter.js
+++ routes/randomModelRouter.js
@@ -16,11 +16,31 @@ router.get("/subProducts", async function (req, res) {
     let subProducts = await subProductModel.find({
       mainProduct: req.query.mainProduct,
     });
-    console.log(subProducts);
     res.render("subProducts", { subProducts: subProducts });
   } catch (error) {
     console.log(error);
   }
+});
+
+router.get("/addToCart", async function (req, res) {
+  let item = await subProductModel.findOne({ _id: req.query.subProduct });
+  let random = await randomModel.findOne();
+  if (!random) {
+    random = new randomModel({ subProducts: [] });
+  }
+  random.subProducts.push(item);
+  await random.save();
+  console.log(random.subProducts);
+});
+
+router.get("/deleteFromCart", async function (req, res) {
+  let item = await subProductModel.findOne({ _id: req.query.subProduct });
+  let random = await randomModel.findOne();
+  random.subProducts = random.subProducts.filter(function (subProduct) {
+    return !subProduct._id.equals(item._id);
+  });
+  await random.save();
+  console.log(random.subProducts);
 });
 
 module.exports = router;
