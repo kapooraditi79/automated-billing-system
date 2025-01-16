9c80f84f89393db3f0a3a26e151633fbf425e2d9
kapooraditi79 <thinktank7997@gmail.com>
2025-01-17 03:39:43 +0530

    Eleventh Changes Made. updated the quantity, add to cart and delete from cart features

--- models/random-model.js
+++ models/random-model.js
@@ -4,8 +4,14 @@ mongoose.connect("mongodb://localhost:27017/part30");
 const randomSchema = mongoose.Schema({
   subProducts: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "subProduct-model",
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "subProduct-model",
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
     },
   ],
 });
--- routes/randomModelRouter.js
+++ routes/randomModelRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const randomModel = require("../models/random-model");
 const subProductModel = require("../models/subProduct-model");
 const mainProductModel = require("../models/mainProduct-model");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -22,25 +23,74 @@ router.get("/subProducts", async function (req, res) {
   }
 });
 
-router.get("/addToCart", async function (req, res) {
-  let item = await subProductModel.findOne({ _id: req.query.subProduct });
+router.post("/addToCart", async function (req, res) {
+  const subProductId = req.body.subProduct;
+  if (!mongoose.Types.ObjectId.isValid(subProductId)) {
+    return res.status(400).send("Invalid subProduct ID");
+  }
+  let item = await subProductModel.findById(subProductId);
   let random = await randomModel.findOne();
   if (!random) {
-    random = new randomModel({ subProducts: [] });
+    random = new randomModel({ subProducts: [], quantity: 0 });
   }
-  random.subProducts.push(item);
+  let subProductInCart = await random.subProducts.find(function (sub) {
+    return sub.product.toString() === subProductId;
+  });
+
+  if (subProductInCart) {
+    subProductInCart.quantity += 1;
+  } else {
+    await random.subProducts.push({ product: item._id, quantity: 1 });
+  }
   await random.save();
-  res.render("addToCart", { subProducts: random.subProducts });
+  // res.redirect("addToCart", { subProducts: random.subProducts });
+  res.json({ success: true, subProducts: random.subProducts });
+});
+
+router.get("/addToCart", async function (req, res) {
+  let random = await randomModel.findOne();
+  if (!random) {
+    return res.status(404).send("Cart not found");
+  }
+  res.render("addToCart", {
+    subProducts: random.subProducts,
+  });
+});
+
+//you match the subProductId with the unique id of the subProduct in the cart
+//because every time the same product is added, mongoDb assigns
+//the same unique_id to that subProduct rather than a new one
+//since each subPrroduct has 3 features assigned to it
+//the unique_id, the product and the quantity
+router.post("/deleteFromCart", async function (req, res) {
+  const subProductId = req.body.subProduct;
+  let random = await randomModel.findOne();
+  if (!random) {
+    return res.status(404).send("Cart not found");
+  }
+  let subProductInCart = await random.subProducts.find(function (sub) {
+    return sub._id.toString() === subProductId;
+  });
+
+  if (subProductInCart.quantity > 1) {
+    subProductInCart.quantity -= 1;
+  } else {
+    random.subProducts = random.subProducts.filter(function (subProduct) {
+      return !(subProduct._id.toString() === subProductId);
+    });
+  }
+  await random.save();
+  res.json({ success: true, subProducts: random.subProducts });
 });
 
 router.get("/deleteFromCart", async function (req, res) {
-  let item = await subProductModel.findOne({ _id: req.query.subProduct });
   let random = await randomModel.findOne();
-  random.subProducts = random.subProducts.filter(function (subProduct) {
-    return !subProduct._id.equals(item._id);
+  if (!random) {
+    return res.status(404).send("Cart not found");
+  }
+  res.render("addToCart", {
+    subProducts: random.subProducts,
   });
-  await random.save();
-  res.render("deleteFromCart", { subProducts: random.subProducts });
 });
 
 module.exports = router;
